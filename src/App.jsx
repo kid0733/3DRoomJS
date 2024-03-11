f7d5bfff827a41ec6f37dd5b95721184a95d65d9
Siddhant Karki <siddhantskarki@gmail.com>
2024-03-12 10:43:43 +1100

    texture color fade animation

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,43 @@ import { Experience } from "./components/Experience";
 import { Scroll, ScrollControls } from "@react-three/drei";
 import { Interface } from "./components/Interface";
 import { ScrollManager } from "./components/ScrollManager";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { Menu } from "./components/Menu";
+import { MotionConfig } from "framer-motion";
 
 
 function App() {
   const [section,setSection]=useState(0)
-
-
+  const [menuOpened, setMenuOpened] = useState(false);
+  useEffect(() => {
+    setMenuOpened(false)
+  }, [section])
+  
 
   return (
     <>
-      <Canvas shadows camera={{ position: [0, 2, 5] }}>
-        <color attach="background" args={["#658ef0"]} />
-        <ScrollControls pages={4} damping={0.1}>
-          <ScrollManager section={section} onSectionChange={setSection}/>
-          <Experience />  
-          <Scroll html>
-            <Interface />
+    <MotionConfig transition={{
+      type:"spring",
+      mass:5,
+      stiffness:500,
+      damping:50,
+      restDelta:0.001,
+    }}>
+        <Canvas shadows camera={{ position: [0, 3, 10], fov: 42 }}>
+          <color attach="background" args={["#658ef0"]} />
+          <ScrollControls pages={4} damping={0.1}>
+            <ScrollManager section={section} onSectionChange={setSection}/>
+            <Scroll>
+              <Experience section={section}/>  
+            </Scroll>
+            <Scroll html>
+              <Interface />
 
-          </Scroll>
-        </ScrollControls>
-      </Canvas>
+            </Scroll>
+          </ScrollControls>
+        </Canvas>
+        <Menu onSectionChange={setSection} menuOpened={menuOpened} setMenuOpened={setMenuOpened}/>
+      </MotionConfig>
     </>
   );
 }
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,12 +1,24 @@
 import { OrbitControls } from "@react-three/drei";
 import { Office } from "./Office";
-
-export const Experience = () => {
+import {motion } from "framer-motion-3d";
+export const Experience = (props) => {
+  const {section}=props
   return (
     <>
       
       <ambientLight intensity={1} />
-      <Office/>
+      <motion.group 
+        position={[1.5,2,3]} 
+        scale={[0.9,0.9,0.9]} 
+        rotation-y={-Math.PI/4}
+        animate={{
+          y:section===0? 0:-1
+        }}
+      > 
+
+        <Office section={section}/>
+
+      </motion.group>
     </>
   );
 };
