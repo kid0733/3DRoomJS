d5fc88dafed526cdc66150c6e45d670a3cf2d61e
Siddhant Karki <siddhantskarki@gmail.com>
2024-03-13 15:44:42 +1100

    minor fixes

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -15,8 +15,9 @@ export const LoadingScreen = (props) => {
     }, [progress, total, loaded, item]);
 
     return (
-        <div
-            className={`fixed top-0 left-0 w-full h-full z-50 transition-opacity duration-1000 pointer-events-none flex items-center justify-center bg-indigo-50 ${started ? "opacity-0" : "opacity-100"}`}
+        <>
+            <div
+            className={`fixed top-0 left-0 w-full h-full z-50 transition-opacity duration-1000 pointer-events-none flex flex-col items-center justify-center bg-indigo-50 ${started ? "opacity-0" : "opacity-100"}`}
         >
             <div className="text-4xl md:text-9xl font-bold text-indigo-900 relative">
                 <div
@@ -29,7 +30,9 @@ export const LoadingScreen = (props) => {
                 </div>
                 <div className="opacity-40">SSK</div>
             </div>
-                <p className="opacity-40">Please wait 3D Models Loading...</p>
+        <p className="opacity-40">Please wait 3D Models Loading...</p>
+        <p className="opacity-40 mt-96">This website is best experience on a computer screen</p>
         </div>
+        </>
     );
 };
